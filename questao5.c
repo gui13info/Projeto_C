4bd7155a10cb350b2129ae4eead0b19562bbc51d
gui13info <55189996+gui13info@users.noreply.github.com>
2019-10-01 09:34:22 -0300

    Add files via upload

---
+++ questao5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "");
+    char texto[8] = "Aula de ";
+    char texto2[41] = "Técnica de Desenvolvimento de Algoritmos";
+    char texto3[100];
+    char texto4[100];
+    int i = 0;
+    for (i = 0; i < sizeof(texto) && texto[i] != '\0'; i++){
+            texto3[i] = texto[i];
+        }
+    for (i = 0; i < sizeof(texto2) && texto2[i] != '\0'; i++){
+            texto4[i] = texto2[i];
+    }
+    printf("%s", texto3);
+    printf("%s", texto4);
+    return 0;
+}
